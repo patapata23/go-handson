9c71c15f00500ba9a88f1ae41532151f5e801953
Masahiro Taguchi <mtaguchi@yahoo-corp.jp>
2017-08-26 15:36:57 +0900

    init

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+
+	"github.com/patapata23/go-handson/fruit"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	d := fruit.GetList()
+	t.templ.Execute(w, d)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "index.html"})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe", err)
+	}
+
+}
